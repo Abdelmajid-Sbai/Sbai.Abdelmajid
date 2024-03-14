1b310246cfbe630421124a6ca73ab4472d1d64fc
Abdelmajid-Sbai <abdosbai2162@gmail.com>
2024-03-14 16:37:12 +0100

    message

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -31,7 +31,7 @@ export default function Home() {
 
         <div className="container home mt-5  mb-5">
             <div className="row ">
-                <div className="col-lg-4  m-2   p-3 mt-5" >
+                <div className="col-lg-5 col-md-6    p-3 mt-5" >
                     <h3 >Hello !<span id="h31" > I'm Abdelmajid S'bai</span></h3>
                     <h3><span style={{ color: 'blue', background:"black", }}>{text}</span><span className="text-red">|</span></h3>
                     <a href="/About" class="btn  mt-5">About Me</a>
@@ -39,7 +39,7 @@ export default function Home() {
 
                     
                 </div>
-                <div className="col-lg-6 border-2 " id='dev-dev-img'>
+                <div className="col-lg-6 col-md-6 border-2 " id='dev-dev-img'>
                     <div ><button className='game'>Hi</button></div>
                     <div className="myphoto " id='devimage'>
                     <img  src={process.env.PUBLIC_URL+"/myphotos/photo1.jpg"} alt="My Photo" className="image"/>
